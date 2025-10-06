07af245f913c076f071f0131b0397d87d095ae21
puchang <puchang-lang@trans-iot.com>
2025-10-06 09:51:09 +0800

    feat: ADASYN algorithm

--- common/_data_balancer.py
+++ common/_data_balancer.py
@@ -1,4 +1,4 @@
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, ADASYN
 from imblearn.under_sampling import RandomUnderSampler
 import pandas as pd
 import warnings
@@ -29,6 +29,29 @@ class Balancer(BaseWithSeed):
             "SMOTE 後樣本數分佈:\n", resampled_y.iloc[:, 0].value_counts().to_string()
         )
         print("✅ SMOTE 過採樣完成。")
+
+        return resampled_x, resampled_y
+
+    def adasyn_resample(self, train_x, train_y, k_neighbors):
+        print("原始樣本數分佈:\n", train_y.value_counts().to_string())
+        adasyn = ADASYN(
+            sampling_strategy="minority",
+            random_state=self.seed,
+            n_neighbors=k_neighbors,
+        )
+
+        try:
+            resampled_x, resampled_y = adasyn.fit_resample(train_x, train_y)
+        except ValueError as e:
+            print(f"❌ ADASYN 執行失敗，請檢查資料：{e}")
+
+        resampled_x = pd.DataFrame(resampled_x, columns=train_x.columns)
+        resampled_y = pd.DataFrame(resampled_y, columns=train_y.columns)
+
+        print(
+            "ADASYN 後樣本數分佈:\n", resampled_y.iloc[:, 0].value_counts().to_string()
+        )
+        print("✅ ADASYN 過採樣完成。")
 
         return resampled_x, resampled_y
 
