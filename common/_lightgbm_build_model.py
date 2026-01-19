a6760a111cabd93b118376b4aebf4b416d710d09
puchang <puchang-lang@trans-iot.com>
2026-01-19 10:17:20 +0800

    feat(lightgbm_build_model): expand Optuna search space and support dynamic class weights for multiclass

--- common/_lightgbm_build_model.py
+++ common/_lightgbm_build_model.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 13 10:17:30 2025
-
-@author: user
-"""
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import (
@@ -24,13 +17,19 @@ warnings.filterwarnings("ignore")
 
 
 class LightGBM(BaseWithSeed):
+    # 注意 python 3.7 以上，參數有序，不要動
     DEFAULT_OPTUNA_PARAMS = {
-        "learning_rate": [0.1, 0.05, 0.01],
-        "max_depth": [3, 5, 7],
-        "subsample": [0.5, 0.7, 0.9],
-        "n_estimators": [500, 1000, 1500],
-        "boosting_type": ["gbdt", "dart"],
-        "bagging_freq": [0, 1, 5],
+        "max_depth": [3, 5, 7, 9],
+        "num_leaves": [15, 31, 63, 127],
+        "learning_rate": [0.01, 0.03, 0.05, 0.1],
+        "n_estimators": [500, 1000, 1500, 2000],
+        "min_data_in_leaf": [20, 50, 100],
+        "lambda_l1": [0, 0.1, 0.5, 1.0],
+        "lambda_l2": [0, 0.1, 0.5, 1.0],
+        "subsample": [0.6, 0.7, 0.8, 0.9],
+        "feature_fraction": [0.6, 0.7, 0.8, 0.9],
+        "bagging_freq": [1, 5],
+        "path_smooth": [0, 0.1, 1.0],
     }
 
     def _get_kfold(self, n_splits, isClassifier, y):
@@ -98,14 +97,39 @@ class LightGBM(BaseWithSeed):
 
         def objective(trial):
             kf = self._get_kfold(n_splits, isClassifier, train_Y)
-            params_tuned = {
-                key: trial.suggest_categorical(key, values)
-                for key, values in current_param_space.items()
-            }
+            params_tuned = {}
+            for key, values in current_param_space.items():
+                if key == "num_leaves":
+                    # def：num_leaves < 2^max_depth
+                    max_depth = params_tuned.get("max_depth", 7)
+                    params_tuned[key] = trial.suggest_int(
+                        key, 2, min(values[-1], 2**max_depth - 1)
+                    )
+                else:
+                    params_tuned[key] = trial.suggest_categorical(key, values)
+
             params_tuned.update({"verbose": -1, "random_state": self.seed})
 
             if isClassifier:
-                model = lgb.LGBMClassifier(**params_tuned)
+                unique_classes = np.unique(train_Y)
+                if len(unique_classes) > 2:
+                    params_tuned["num_class"] = len(unique_classes)
+                    # 不平衡處理：動態搜權重 = 0固定為1，剩餘找 1.0~10.0 倍
+                    class_weight = {0: 1.0}
+                    for c in unique_classes:
+                        if c != 0:
+                            class_weight[c] = trial.suggest_float(
+                                f"weight_class_{c}", 1.0, 10.0
+                            )
+
+                    model = lgb.LGBMClassifier(
+                        **params_tuned, class_weight=class_weight
+                    )
+                else:
+                    # 二元分類：scale_pos_weight 正(1)是負(0)的幾倍
+                    s_weight = trial.suggest_float("scale_pos_weight", 1.0, 10.0)
+                    model = lgb.LGBMClassifier(**params_tuned)
+
                 score = cross_val_score(
                     model, train_X, train_Y, cv=kf, scoring="f1_macro"
                 ).mean()
@@ -131,9 +155,14 @@ class LightGBM(BaseWithSeed):
             best_params["verbose"] = -1
 
             if isClassifier:
-                num_classes = len(np.unique(train_Y))
-                best_params["num_class"] = num_classes
-
+                weights = {0: 1.0}
+                for c in np.unique(train_Y):
+                    if c != 0:
+                        key = f"weight_class_{c}"
+                        weights[c] = best_params.pop(key)
+                best_params["class_weight"] = weights
+                best_params["num_class"] = len(np.unique(train_Y))
+                # scale_pos_weight 會直接在 best_params 中，不需額外處理
             return best_params
 
         except Exception as e:
